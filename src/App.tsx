786da9c537ebe7964e866bc9245fea44a67f3587
m-nelsen <mattnelsen95@gmail.com>
2024-04-04 16:33:16 -0700

    Overriding default bootstrap colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import "./scss/styles.scss";
 function App() {
   return (
     <>
-      <div className="container">
-        <p>App</p>
-        <button className="btn btn-danger">Button</button>
+      <nav className="nav flex-column">
+        <a className="nav-link active" aria-current="page" href="#">
+          Active
+        </a>
+        <a className="nav-link" href="#">
+          Link
+        </a>
+        <a className="nav-link" href="#">
+          Link
+        </a>
+        <a className="nav-link disabled" aria-disabled="true">
+          Disabled
+        </a>
+      </nav>
+      <div className="container d-flex flex-column w-25 gap-3">
+        <button className="btn btn-light">Light</button>
+        <button className="btn btn-dark">Dark</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-danger">Danger</button>
       </div>
     </>
   );
